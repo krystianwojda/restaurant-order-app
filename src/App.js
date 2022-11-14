04602dda05c7920b0f08db23462379ce4dbe764b
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-14 12:18:24 +0100

    Changing style in Cart.module.css Create component Modal, Added style to him, create portal to index.html,

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Fragment} from "react";
 import Header from "./components/Layout/Header";
 import AvailableMeals from "./components/Meals/AvailableMeals";
 import Banner from "./components/Layout/Banner";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
       <Fragment>
+          <Cart/>
           <Header/>
           <Banner/>
           <main>
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,12 @@
+import Modal from "../UI/Modal";
+
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
     const cartItem = <ul className={classes['cart-items']}>{[{id:'c1',name:'Sushi',amount: 2,price: 39.99},].map(item => <li>{item.name}</li>)}</ul>;
 
     return(
-        <div>
+        <Modal>
             {cartItem}
             <div className={classes.total}>
                 <span>Kwota to zapłaty</span>
@@ -14,7 +16,7 @@ const Cart = (props) => {
                 <button className={classes['button--alt']}>Zamknij</button>
                 <button className={classes.button}>Zamów</button>
             </div>
-        </div>
+        </Modal>
     );
 };
 
