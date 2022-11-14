8eab84c7ced2f654a51ba6c92237b48dfe72dbf9
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-14 12:59:07 +0100

    Added open Cart,

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import Header from "./components/Layout/Header";
 import AvailableMeals from "./components/Meals/AvailableMeals";
 import Banner from "./components/Layout/Banner";
 import Cart from "./components/Cart/Cart";
 
 function App() {
-  return (
-      <Fragment>
-          <Cart/>
-          <Header/>
-          <Banner/>
-          <main>
-              <AvailableMeals/>
-          </main>
-      </Fragment>
-  );
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    };
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    };
+
+    return (
+        <Fragment>
+            {cartIsShown && <Cart/>}
+            <Header onShowCart={showCartHandler}/>
+            <Banner/>
+            <main>
+                <AvailableMeals/>
+            </main>
+       </Fragment>
+    );
 }
 
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     return(
         <header className={classes.header}>
             <h1>Logo firmy</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
     );
 };
