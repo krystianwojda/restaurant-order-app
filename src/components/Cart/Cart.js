96049a1fa23d9a173cdd84a8298eb5b8016410ff
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-14 13:07:59 +0100

    Added close Card to button and window modal, Fix bug name

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart = (props) => {
     const cartItem = <ul className={classes['cart-items']}>{[{id:'c1',name:'Sushi',amount: 2,price: 39.99},].map(item => <li>{item.name}</li>)}</ul>;
 
     return(
-        <Modal>
+        <Modal onClose={props.onClose}>
             {cartItem}
             <div className={classes.total}>
-                <span>Kwota to zapłaty</span>
+                <span>Kwota do zapłaty</span>
                 <span>39.99</span>
             </div>
             <div className={classes.actions}>
-                <button className={classes['button--alt']}>Zamknij</button>
+                <button className={classes['button--alt']} onClick={props.onClose}>Zamknij</button>
                 <button className={classes.button}>Zamów</button>
             </div>
         </Modal>
