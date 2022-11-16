e0e13ecf4e78aab8e92b44ea89445d904519f5ae
krystian.wojda <krystian.wojda-98@wp.pl>
2022-11-16 14:49:27 +0100

    Create logic remove meal from the Cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -30,6 +30,23 @@ const cartReducer = (state, action) => {
             totalAmount: updatedTotalAmount
         };
     }
+    if (action.type === "REMOVE") {
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+        if (existingItems.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
     return defaultCartState;
 };
 
